1fbabe21d3e2c51fca3e46ea39836bba933c2aa4
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-14 02:28:29 +0300

    Define constants

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+//Assign constants 
+    //Host
+    define("HOST", "localhost");
+    //dbname
+    define("DBNAME", "GuzoGateways");
+    //Username 
+    define("USERNAME", "root");
+    //Password
+    define("PASS", "");
+
+
+?>
