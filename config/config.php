0595f34395eae1735189c648ce1a8a75c5b8422e
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-14 03:09:03 +0300

    I wanna restart all

--- config/config.php
+++ config/config.php
@@ -1,13 +1,23 @@
 <?php
 //Assign constants 
+echo  "Hey there";
     //Host
     define("HOST", "localhost");
     //dbname
     define("DBNAME", "GuzoGateways");
     //Username 
-    define("USERNAME", "root");
+    define("USER", "root");
     //Password
     define("PASS", "");
+
+    // Create connection using PDO
+    $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASS);
+    if ($conn == true) {
+        echo "Connection established";
+    } else {
+        echo "Connection failed";
+    }
+
 
 
 ?>
